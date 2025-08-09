a18da87b1671784df152fdf5e7c6cf2443bc9457
naimulislamsaikat <nzsaikat06@gmail.com>
2025-08-09 21:19:24 +0600

    text formatting

---
+++ Text Formatting.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <p><b>Paragraph text is in bold</b></p>
+        <p><i>Paragraph text is in italic</i></p>
+        <p>This is<sub> subscript</sub> and <sup>superscript</sup></p>
+        <p>Do not forget to learn <mark>HTML</mark> programming.</p>
+        <p>My favorite color is <del>blue</del> red.</p>
+        <p>My favorite color is <del>blue</del> <ins>red</ins>.</p>
+    </body>
+</html>
