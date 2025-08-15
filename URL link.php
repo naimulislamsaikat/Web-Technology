fe14ff553ffa4037d36e07e99ba17be9a36a77a8
naimulislamsaikat <nzsaikat06@gmail.com>
2025-08-15 21:23:51 +0600

    connect URL linking functionality to visit different web pages

---
+++ URL link.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h2>Open URL in new window/page</h2>
+        <p><a href="https://www.w3.org/" target="_blank">W3 School</a></p>
+        <p><a href="https://www.google.com/" target="_blank">Google</a></p>
+
+        <h2>Open URL in existing page</h2>
+        <p><a href="html_images.asp">HTML Images</a></p>
+        <p><a href="https://www.w3schools.com/html/html_links.asp">HTML Links</a></p>
+
+    </body>
+</html>
